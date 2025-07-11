bed3e37e780dfab02d903bea56f92968ff09c8e7
AlvaroLS01 <136437203+AlvaroLS01@users.noreply.github.com>
2025-07-11 13:34:35 +0200

    Revert "Implement FT correction refactor"

---
+++ comerzzia-bricodepot-motivos-linea-services/src/main/java/com/comerzzia/bricodepot/motivoslinea/model/bbdd/Albaran.java
@@ -0,0 +1,44 @@
+package com.comerzzia.bricodepot.motivoslinea.model.bbdd;
+
+
+public class Albaran {
+	
+	private String codAlbaran;
+	
+	private String codAlbaranOrigen;
+	
+	private String uidTicket;
+
+	
+	public String getCodAlbaran() {
+		return codAlbaran;
+	}
+
+	
+	public void setCodAlbaran(String codAlbaran) {
+		this.codAlbaran = codAlbaran;
+	}
+
+	
+	public String getCodAlbaranOrigen() {
+		return codAlbaranOrigen;
+	}
+
+	
+	public void setCodAlbaranOrigen(String codAlbaranOrigen) {
+		this.codAlbaranOrigen = codAlbaranOrigen;
+	}
+
+	
+	public String getUidTicket() {
+		return uidTicket;
+	}
+
+	
+	public void setUidTicket(String uidTicket) {
+		this.uidTicket = uidTicket;
+	}
+	
+	
+
+}
---
+++ comerzzia-bricodepot-motivos-linea-services/src/main/java/com/comerzzia/bricodepot/motivoslinea/model/xml/MotivoTicket.java
@@ -0,0 +1,82 @@
+package com.comerzzia.bricodepot.motivoslinea.model.xml;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class MotivoTicket {
+
+	@XmlElement(name = "uidActividad")
+	private String uidActividad;
+	@XmlElement(name = "codigo")
+	private String codigo;
+	@XmlElement(name = "codigo_tipo")
+	private String codigoTipo;
+	@XmlElement(name = "descripcion")
+	private String descripcion;
+	@XmlElement(name = "comentario")
+	private String comentario;
+	@XmlElement(name = "precio_articulo_original")
+	private String precioArticuloOriginal;
+	@XmlElement(name = "precio_articulo_aplicado")
+	private String precioArticuloAplicado;
+	
+	public String getUidActividad() {
+		return uidActividad;
+	}
+	
+	public void setUidActividad(String uidActividad) {
+		this.uidActividad = uidActividad;
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getCodigoTipo() {
+		return codigoTipo;
+	}
+	
+	public void setCodigoTipo(String codigoTipo) {
+		this.codigoTipo = codigoTipo;
+	}
+	
+	public String getDescripcion() {
+		return descripcion;
+	}
+	
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	public String getComentario() {
+		return comentario;
+	}
+	
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+	
+	public String getPrecioArticuloOriginal() {
+		return precioArticuloOriginal;
+	}
+	
+	public void setPrecioArticuloOriginal(String precioArticuloOriginal) {
+		this.precioArticuloOriginal = precioArticuloOriginal;
+	}
+	
+	public String getPrecioArticuloAplicado() {
+		return precioArticuloAplicado;
+	}
+	
+	public void setPrecioArticuloAplicado(String precioArticuloAplicado) {
+		this.precioArticuloAplicado = precioArticuloAplicado;
+	}
+	
+	
+}
