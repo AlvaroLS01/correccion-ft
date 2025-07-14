959b44d9d4964544b8d97f2acbcd9cf277542bba
AlvaroLS01 <136437203+AlvaroLS01@users.noreply.github.com>
2025-07-14 08:33:09 +0200

    Reemplazar JAXB por DOM en servicio de correcciones

--- generar-ft-de-fs-services/src/main/java/com/comerzzia/bricodepot/generarftdefs/services/correccion/CorreccionFtService.java
+++ generar-ft-de-fs-services/src/main/java/com/comerzzia/bricodepot/generarftdefs/services/correccion/CorreccionFtService.java
@@ -1,23 +1,31 @@
 package com.comerzzia.bricodepot.generarftdefs.services.correccion;
 
-import com.comerzzia.bricodepot.generarftdefs.model.xml.LineaTicket;
-import com.comerzzia.bricodepot.generarftdefs.model.xml.TicketXml;
 import com.comerzzia.bricodepot.generarftdefs.persistence.ProveedorConexion;
 import com.comerzzia.bricodepot.generarftdefs.persistence.TicketsDao;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -45,10 +53,6 @@ public class CorreccionFtService {
         Connection conexion = null;
         try {
             conexion = proveedorConexion.obtenerConexion();
-            JAXBContext context = JAXBContext.newInstance(TicketXml.class);
-            Unmarshaller unmarshaller = context.createUnmarshaller();
-            Marshaller marshaller = context.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
             String linea = reader.readLine();
             // Saltamos cabecera
@@ -64,63 +68,103 @@ public class CorreccionFtService {
                 String uidFt = partes[2].trim();
 
                 try {
-                    TicketXml fs = obtenerTicket(conexion, unmarshaller, uidFs);
-                    TicketXml ft = obtenerTicket(conexion, unmarshaller, uidFt);
+                    Document fs = obtenerTicket(conexion, uidFs);
+                    Document ft = obtenerTicket(conexion, uidFt);
                     if (fs != null && ft != null) {
                         corregirTicket(fs, ft);
-                        String xmlCorregido = marshalTicket(marshaller, ft);
+                        String xmlCorregido = marshalTicket(ft);
                         TicketsDao.actualizarTicket(conexion, uidActividad, uidFt, xmlCorregido);
                     }
                 } catch (Exception e) {
                     log.error("Error procesando la fila para {}: {}", uidFt, e.getMessage(), e);
                 }
             }
-        } catch (IOException | JAXBException e) {
+        } catch (IOException e) {
             log.error("Error procesando CSV: " + e.getMessage(), e);
         }
     }
 
-    private TicketXml obtenerTicket(Connection conexion, Unmarshaller unmarshaller, String uidTicket)
-            throws SQLException, JAXBException {
+    private Document obtenerTicket(Connection conexion, String uidTicket)
+            throws SQLException, ParserConfigurationException, IOException, SAXException {
         ResultSet rs = TicketsDao.consultarTicketPorUid(conexion, uidActividad, uidTicket);
-        TicketXml ticket = null;
+        Document doc = null;
         if (rs.next()) {
             String xml = rs.getString("ticket");
             xml = sanitizeXml(xml);
-            ticket = (TicketXml) unmarshaller.unmarshal(new StringReader(xml));
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setNamespaceAware(true);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            doc = builder.parse(new InputSource(new StringReader(xml)));
         }
         rs.close();
-        return ticket;
+        return doc;
     }
 
-    private String marshalTicket(Marshaller marshaller, TicketXml ticket) throws JAXBException {
-        StringWriter sw = new StringWriter();
-        marshaller.marshal(ticket, sw);
-        return sw.toString();
+    private String marshalTicket(Document ticket) throws TransformerException {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        StringWriter writer = new StringWriter();
+        transformer.transform(new DOMSource(ticket), new StreamResult(writer));
+        return writer.toString();
     }
 
     /**
      * Corrige la información del ticket FT copiando los datos de la FS asociada.
      */
-    private void corregirTicket(TicketXml fs, TicketXml ft) {
-        ft.getCabeceraTicket().setLineasImpuestos(fs.getCabeceraTicket().getLineasImpuestos());
-        ft.getCabeceraTicket().setTotales(fs.getCabeceraTicket().getTotales());
-        ft.setPagos(fs.getPagos());
+    private void corregirTicket(Document fs, Document ft) {
+        Element cabeceraFs = (Element) fs.getElementsByTagName("cabecera").item(0);
+        Element cabeceraFt = (Element) ft.getElementsByTagName("cabecera").item(0);
+        if (cabeceraFs != null && cabeceraFt != null) {
+            copiarNodo(cabeceraFs, cabeceraFt, "lineasimpuestos");
+            copiarNodo(cabeceraFs, cabeceraFt, "totales");
+        }
 
-        List<LineaTicket> lineasFs = fs.getLineas();
-        List<LineaTicket> lineasFt = ft.getLineas();
-        int size = Math.min(lineasFs.size(), lineasFt.size());
+        Element pagosFs = (Element) fs.getElementsByTagName("pagos").item(0);
+        Element pagosFt = (Element) ft.getElementsByTagName("pagos").item(0);
+        if (pagosFs != null && pagosFt != null) {
+            Node nuevo = ft.importNode(pagosFs, true);
+            pagosFt.getParentNode().replaceChild(nuevo, pagosFt);
+        }
+
+        NodeList lineasFs = fs.getElementsByTagName("linea");
+        NodeList lineasFt = ft.getElementsByTagName("linea");
+        int size = Math.min(lineasFs.getLength(), lineasFt.getLength());
         for (int i = 0; i < size; i++) {
-            LineaTicket lfs = lineasFs.get(i);
-            LineaTicket lft = lineasFt.get(i);
-            lft.setPrecioTarifaOrigen(lfs.getPrecioTarifaOrigen());
-            lft.setPrecioTotalTarifaOrigen(lfs.getPrecioTotalTarifaOrigen());
-            lft.setPrecioSinDto(lfs.getPrecioSinDto());
-            lft.setPrecioTotalSinDto(lfs.getPrecioTotalSinDto());
-            lft.setPrecio(lfs.getPrecio());
-            lft.setPrecioTotal(lfs.getPrecioTotal());
-            lft.setImporte(lfs.getImporte());
-            lft.setImporteTotal(lfs.getImporteTotal());
+            Element lfs = (Element) lineasFs.item(i);
+            Element lft = (Element) lineasFt.item(i);
+            copiarTexto(lfs, lft, "precio_tarifa_origen");
+            copiarTexto(lfs, lft, "precio_total_tarifa_origen");
+            copiarTexto(lfs, lft, "precio_sin_dto");
+            copiarTexto(lfs, lft, "precio_total_sin_dto");
+            copiarTexto(lfs, lft, "precio");
+            copiarTexto(lfs, lft, "precio_total");
+            copiarTexto(lfs, lft, "importe");
+            copiarTexto(lfs, lft, "importe_total");
+        }
+    }
+
+    private void copiarNodo(Element origen, Element destino, String nombre) {
+        NodeList lista = origen.getElementsByTagName(nombre);
+        if (lista.getLength() == 0) {
+            return;
+        }
+        Node nodoOrigen = lista.item(0);
+        Node nodoImportado = destino.getOwnerDocument().importNode(nodoOrigen, true);
+        NodeList existentes = destino.getElementsByTagName(nombre);
+        if (existentes.getLength() > 0) {
+            destino.replaceChild(nodoImportado, existentes.item(0));
+        } else {
+            destino.appendChild(nodoImportado);
+        }
+    }
+
+    private void copiarTexto(Element origen, Element destino, String nombre) {
+        NodeList fromList = origen.getElementsByTagName(nombre);
+        NodeList toList = destino.getElementsByTagName(nombre);
+        if (fromList.getLength() > 0 && toList.getLength() > 0) {
+            toList.item(0).setTextContent(fromList.item(0).getTextContent());
         }
     }
 
